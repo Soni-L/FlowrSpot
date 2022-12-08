f35d987120142342d29dff0f272eb2d3137d0191
soni <acdsoni@gmail.com>
2022-12-08 18:55:46 +0100

    created modal

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import SignupModal from "../SignupModal/Signup";
 import "./Navbar.css";
 
 export default function Navbar() {
+  const [signupModal, setSignupModal] = useState(false);
   return (
-    <nav className="navbar-container">
-      <div style={{ flexGrow: 1, display: "flex", gap: "4px" }}>
-        <img src="/logo.svg" />
-        <a
-          href="#"
-          style={{ fontWeight: 700, fontSize: "20px", color: "#EAA79E" }}
-        >
-          FlowrSpot
-        </a>
-      </div>
+    <>
+      <nav className="navbar-container">
+        <div style={{ flexGrow: 1, display: "flex", gap: "4px" }}>
+          <img src="/logo.svg" />
+          <a
+            href="#"
+            style={{ fontWeight: 700, fontSize: "20px", color: "#EAA79E" }}
+          >
+            FlowrSpot
+          </a>
+        </div>
 
-      <a href="#">Flowers</a>
-      <a href="#">Latest Sightings</a>
-      <a href="#">Favorites</a>
-      <a>Login</a>
-      <button id="signup-modal">New Account</button>
-    </nav>
+        <a href="#">Flowers</a>
+        <a href="#">Latest Sightings</a>
+        <a href="#">Favorites</a>
+        <a>Login</a>
+        <button onClick={() => setSignupModal(true)} className="signup-button">
+          New Account
+        </button>
+      </nav>
+      <SignupModal open={signupModal} onClose={() => setSignupModal(false)} />
+    </>
   );
 }
--- src/components/SignupModal/Signup.jsx
+++ src/components/SignupModal/Signup.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import ReactDom from "react-dom";
 
-export default function Signup() {
-  return (
-    <div>Signup</div>
-  )
+const MODAL_STYLES = {
+  position: "fixed",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  backgroundColor: "#FFF",
+  zIndex: 1000,
+};
+
+const OVERLAY_STYLES = {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: "rgba(0, 0, 0, .7)",
+  zIndex: 1000,
+};
+
+export default function Signup({ open, onClose }) {
+  const [documentLoaded, setDocumentLoaded] = useState(false);
+
+  useEffect(() => {
+    setDocumentLoaded(true);
+  }, []);
+
+  if (!open || !documentLoaded) return <></>;
+  return ReactDom.createPortal(
+    <>
+      <div style={OVERLAY_STYLES} onClick={onClose} />
+      <div style={MODAL_STYLES}>
+        <div style={{ height: "100px", width: "100px" }}>hello modal</div>
+      </div>
+    </>,
+    document.getElementById("portal")
+  );
 }
